46f34583d136af7ee09c087c82c71bfc79e72262
Vladimir Drannikov <krowen@inbox.ru>
2021-01-14 15:35:50 +0600

    Added locales

---
+++ src/locales/en.js
@@ -0,0 +1,24 @@
+export default {
+  translation: {
+    appName: 'RSS Reader',
+    appLead: 'Start reading RSS today! It is easy, it is nicely.',
+    appExample: 'Example: https://ru.hexlet.io/lessons.rss',
+    appCopy: 'Vladimir Drannikov',
+    rssForm: {
+      placeholder: 'RSS link',
+      button: 'Submit',
+      feedback: {
+        success: 'RSS has been loaded',
+        double: 'This RSS already exist',
+        networkError: 'Network error',
+        required: 'This is required field',
+        url: 'Must be valid URL',
+      },
+    },
+    rssList: {
+      feedsTitle: 'Feeds',
+      postsTitle: 'Posts',
+      button: 'Preview',
+    },
+  },
+};
