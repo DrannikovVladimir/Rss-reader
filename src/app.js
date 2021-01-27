cc1687aea596a8768857cebce7f09ab462cbdbe0
Vladimir Drannikov <krowen@inbox.ru>
2021-01-27 15:58:29 +0600

    Added debag print

--- src/app.js
+++ src/app.js
@@ -78,6 +78,7 @@ export default () => {
     evt.preventDefault();
     const formData = new FormData(evt.target);
     const url = formData.get('url');
+    console.log(watched);
     const { feeds } = watched;
     validate(url, feeds)
       .then(() => {
