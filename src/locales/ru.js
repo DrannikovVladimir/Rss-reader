5c958216a242460dd63be5150d9e2bb855153e5c
Vladimir Drannikov <krowen@inbox.ru>
2021-02-18 10:36:26 +0600

    Added russian text

---
+++ src/locales/ru.js
@@ -0,0 +1,29 @@
+export default {
+  translation: {
+    appName: 'RSS Читалка',
+    appLead: 'Начните читать RSS сегодня! Это просто, это красиво.',
+    appExample: 'Пример: https://ru.hexlet.io/lessons.rss',
+    appCopy: 'Владимир Дранников',
+    rssForm: {
+      placeholder: 'RSS ссылка',
+      button: 'Добавить',
+      feedback: {
+        success: 'RSS успешно загружен',
+        double: 'Rss уже существует',
+        networkError: 'Ошибка сети',
+        required: 'Это обязательное поле',
+        url: 'Ссылка должна быть валидным URL',
+        validRss: 'Ресурс не содержит валидный RSS',
+      },
+    },
+    rssList: {
+      feedsTitle: 'Каналы',
+      postsTitle: 'Посты',
+      button: 'Посмотреть',
+    },
+    modal: {
+      link: 'Открыть',
+      close: 'Закрыть',
+    },
+  },
+};
